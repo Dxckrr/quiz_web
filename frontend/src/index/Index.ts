59d11a3f4d1de8ff949f3500e7fb37e14b5d9d78
unknown <admin@BGAI201-017.bga.upb>
2025-04-24 17:31:30 -0500

    async

--- frontend/src/index/Index.ts
+++ frontend/src/index/Index.ts
@@ -4,4 +4,13 @@ import IndexView from "./view/IndexView.js";
 export default class Index {
     private readonly indexView: IndexView;
     private readonly IndexController: IndexController;
+
+    constructor() {
+        this.indexView = new IndexView();
+        this.IndexController = new IndexController(this.indexView);
+    }
+
+    readonly init = () => {
+        this.IndexController.init();
+    }
 }
