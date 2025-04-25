be390b1451ba6a3f0134db228773dffd29db9218
Dxckrr <juandavidp1127@gmail.com>
2025-04-25 15:05:09 -0500

    feat search functionality!

--- frontend/build/js/searchbar/view/SearchbarView.js
+++ frontend/build/js/searchbar/view/SearchbarView.js
@@ -4,23 +4,29 @@ export default class SearchbarView {
     SearchbarHTML;
     constructor(searchBooksFn) {
         this.searchBooksFn = searchBooksFn;
-        this.SearchbarHTML = document.createElement('searchbar');
-        this.SearchbarHTML.classList.add('search-input');
+        this.SearchbarHTML = document.createElement("searchbar");
+        this.SearchbarHTML.classList.add("search-input");
     }
     init = () => {
-        console.log('SearchbarView initialized');
-        this.SearchbarHTML.addEventListener('submit', async (e) => {
-            e.preventDefault();
-            this.searchBooks();
-        });
+        console.log("SearchbarView initialized");
         this.render();
     };
     render = () => {
         const template = new SearchbarTemplate();
         this.SearchbarHTML.innerHTML = template.render();
+        this.handleSearch();
+    };
+    handleSearch = () => {
+        const button = this.SearchbarHTML.querySelector("#search-button");
+        button.addEventListener("click", async (e) => {
+            e.preventDefault();
+            console.log("button clicked");
+            await this.searchBooks();
+        });
     };
     searchBooks = async () => {
-        const input = this.SearchbarHTML.querySelector('input');
+        console.log("searchBooks called");
+        const input = this.SearchbarHTML.querySelector(".search-input");
         const search = input.value;
         await this.searchBooksFn(search);
         this.render();
--- frontend/src/searchbar/view/SearchbarView.ts
+++ frontend/src/searchbar/view/SearchbarView.ts
@@ -1,36 +1,46 @@
 import SearchbarTemplate from "../template/SearchbarTemplate.js";
 
 export default class SearchbarView {
-    private readonly SearchbarHTML;
+  private readonly SearchbarHTML;
 
-    constructor(readonly searchBooksFn: (search: string) => Promise<void> ) {
-        this.SearchbarHTML = document.createElement('searchbar') as HTMLElement;
-        this.SearchbarHTML.classList.add('search-input');
-    }
+  constructor(readonly searchBooksFn: (search: string) => Promise<void>) {
+    this.SearchbarHTML = document.createElement("searchbar") as HTMLElement;
+    this.SearchbarHTML.classList.add("search-input");
+  }
 
-    readonly init = () => {
-        console.log('SearchbarView initialized');
-        this.SearchbarHTML.addEventListener('submit', async (e) => {
-            e.preventDefault();
-            this.searchBooks();
-        })
-        this.render();
-    }
+  readonly init = () => {
+    console.log("SearchbarView initialized");
+    this.render();
+  };
+  readonly render = () => {
+    const template = new SearchbarTemplate();
+    this.SearchbarHTML.innerHTML = template.render();
+    this.handleSearch();
+  };
 
-    readonly render = () => {
-        const template = new SearchbarTemplate();
-        this.SearchbarHTML.innerHTML = template.render();
-    }
+  readonly handleSearch = () => {
+    const button = this.SearchbarHTML.querySelector(
+      "#search-button"
+    ) as HTMLButtonElement;
+    button.addEventListener("click", async (e) => {
+      e.preventDefault();
+      console.log("button clicked");
+      await this.searchBooks();
+    });
+  };
 
-    readonly searchBooks = async () => {
-        const input = this.SearchbarHTML.querySelector('input') as HTMLInputElement;
-        const search = input.value;
+  readonly searchBooks = async () => {
+    console.log("searchBooks called");
+    const input = this.SearchbarHTML.querySelector(
+      ".search-input"
+    ) as HTMLInputElement;
+    const search = input.value;
 
-        await this.searchBooksFn(search);
-        this.render();
-    }
+    await this.searchBooksFn(search);
+    this.render();
+  };
 
-    readonly getSearchbarHTML = (): HTMLElement => {
-        return this.SearchbarHTML;
-    }
+  readonly getSearchbarHTML = (): HTMLElement => {
+    return this.SearchbarHTML;
+  };
 }
